932b52d69a7914df8c3f6eb8701d334dab0213b9
yash03112000 <yashag@iitk.ac.in>
2021-01-16 17:15:41 +0530

    replaced uglify with terserjs compatible with es6 and webpack4

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -11,16 +12,8 @@ module.exports = merge(commonConfig, {
     filename: 'js/[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
-
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      },
-      output: {
-        comments: false
-      }
-    })
-  ]
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  }
 });
